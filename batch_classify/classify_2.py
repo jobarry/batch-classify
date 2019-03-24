f7e1db193b15825045dda9dea83ae3fa91f45ad0
John Barry <jbarry2@cisco.com>
2019-03-24 21:08:58 +0000

    classify status script

---
+++ batch_classify/classify_2.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import datetime
+
+#data import
+transactions = pd.read_csv("~/Desktop/eightysix/transactions.csv")
+cust_products = pd.read_csv("~/Desktop/eightysix/customer_products.csv")
+today = datetime.date.today()
+
+#convert dates and merge datasets
+transactions['delivered'] = pd.to_datetime(transactions['delivered']).dt.date
+cust_products['last_delivered'] = pd.to_datetime(cust_products['last_delivered']).dt.date
+structured_transactions=transactions.sort_values(['customer_id','product_id','delivered'],ascending=True)
+structured_transactions=structured_transactions.merge(cust_products[['customer_id','product_id','last_delivered']], left_on=['customer_id','product_id'], right_on=['customer_id','product_id'], how='left')
+
+#group by and shift to align delivery dates to prior delivery dates
+grouped=t.groupby(['customer_id','product_id','delivered'])['outlier','delivered','cost','price','quantity'].last()
+grouped['last_delivered'] = grouped.groupby([grouped.index.get_level_values('customer_id'),'product_id'])['delivered'].shift()
+
+grouped['last_delivered']=grouped['last_delivered'].fillna(grouped['delivered'])
+grouped['gap']=(grouped['delivered'] - grouped['last_delivered']).dt.days
+
+def calculate_transaction_status(gap, outlier):
+    if gap==0:
+        return "new"
+    if gap>outlier*2:
+        return "lost"
+    elif gap>outlier:
+        return "opportunity"
+    elif gap<outlier:
+        return "return"
+
+grouped['status'] = np.vectorize(calculate_transaction_status)(grouped['gap'], grouped['outlier'])
+
+#need last row
+def calculate_last_transaction(lr):
+    if lr[0] + datetime.timedelta(lr[1]*2)<today and lr[0]!=lr[0]:
+        return 'lost'
+    elif lr[0] + datetime.timedelta(lr[1])<today and lr[0]!=lr[0]:
+        return 'opportunity'
+    else:
+        return lr[2]
+
+grouped.loc[grouped.groupby(['customer_id','product_id'])['delivered'].tail(1).index, 'status'] = grouped[['delivered','outlier','status']].apply(calculate_last_transaction, axis=1)
+grouped.to_csv('~/Desktop/eightysix/transactions_status.csv', sep=',', encoding='utf-8',index=True)
